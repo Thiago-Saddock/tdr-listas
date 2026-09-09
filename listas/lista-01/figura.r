3b8b63bf937c9f8b8c3353b77a1c016e4628980a
Thiago Saddock <thiago56sa@gmail.com>
2026-09-09 20:27:39 -0300

    Exercício 3 - pt1: Fiz novamente, acredito que o .r devia ser minúsculo

---
+++ listas/lista-01/figura.r
@@ -0,0 +1,10 @@
+air <- read.csv("airquality.csv")
+
+pdf("figura.pdf", width = 7, height = 5)
+
+boxplot(Temp ~ Month, data = air,
+        main = "Distribuição de Temperatura por mês",
+        xlab = "Mês",
+        ylab = "Fahrenheit")
+
+dev.off()
