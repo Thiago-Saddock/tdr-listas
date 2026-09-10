87c32cd2a9415adfbde3042f28830de6ce12bf9d
Thiago Saddock <thiago56sa@gmail.com>
2026-09-09 21:50:48 -0300

    Exercício 3 - Pt1

--- listas/lista-01/figura.r
+++ listas/lista-01/figura.r
@@ -1,12 +1,13 @@
+library(ggplot2)
 air <- read.csv("airquality.csv")
 
-pdf("figura.pdf", width = 7, height = 5)
+figura <- ggplot(air, aes(x = as.factor(Month), y = Temp)) +
+  geom_boxplot(fill = "lightgreen", color = "darkgreen") +
+  labs(
+    title = "Distribuição da Temperatura por Mês",
+    x = "Mês",
+    y = "valores"
+  ) +
+theme_minimal()
 
-boxplot(Temp ~ Month,
-  data = air,
-  main = "Distribuição de Temperatura por mês",
-  xlab = "Mês",
-  ylab = "Fahrenheit"
-)
-
-dev.off()
+ggsave("figura.pdf", plot = figura, width = 7, height = 5)
